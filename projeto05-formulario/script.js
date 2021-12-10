cf30c636ac83173560da0f7b76edd24ccf4f8477
guilherme-bravo <84751900+guilherme-bravo@users.noreply.github.com>
2021-12-09 22:04:50 -0300

    adicionando todos projetos feitos com javascript

---
+++ projeto05-formulario/script.js
@@ -0,0 +1,62 @@
+const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.getElementById("progress");
+const formSteps = document.querySelectorAll(".form-step");
+const progressSteps = document.querySelectorAll(".progress-step");
+
+let formStepsNum = 0;
+
+nextBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        formStepsNum++;
+        updateFormSteps();
+        updateProgressbar();
+    });
+});
+
+prevBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        formStepsNum--;
+        updateFormSteps();
+        updateProgressbar();
+    });
+});
+
+function updateFormSteps() {
+    formSteps.forEach((formStep) => {
+        formStep.classList.contains("form-step-active") &&
+            formStep.classList.remove("form-step-active");
+    });
+
+    formSteps[formStepsNum].classList.add("form-step-active");
+}
+
+function updateProgressbar() {
+    progressSteps.forEach((progressStep, idx) => {
+        if (idx < formStepsNum + 1) {
+            progressStep.classList.add("progress-step-active");
+        } else {
+            progressStep.classList.remove("progress-step-active");
+        }
+    });
+
+    const progressActive = document.querySelectorAll(".progress-step-active");
+
+    progress.style.width =
+        ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
+}
+
+//TROCANDO O FORMATO DA DATA NO INPUT
+
+function DDMMYYYY(value, event) {
+    let newValue = value.replace(/[^0-9]/g, '').replace(/(\..*)\./g, '$1');
+  
+    const dayOrMonth = (index) => index % 2 === 1 && index < 4;
+  
+    // on delete key.  
+    if (!event.data) {
+      return value;
+    }
+  
+    return newValue.split('').map((v, i) => dayOrMonth(i) ? v + '/' : v).join('');;
+  }
---
+++ projeto06-urna/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: 'Carlinhos Goiaba',
+                partido: 'Goiaba',
+                fotos: [
+                    { url: '6.jpg', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Marquin Pineu',
+                partido: 'Goiaba',
+                fotos: [
+                    { url: '5.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'Michael',
+                partido: 'GTA',
+                vice: 'Franklin',
+                fotos: [
+                    { url: '2.png', legenda: 'Prefeito' },
+                    { url: '3.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '20',
+                nome: 'NeyNey',
+                partido: 'PeixaoFC',
+                vice: 'Pele',
+                fotos: [
+                    { url: '1.png', legenda: 'Prefeito' },
+                    { url: '4.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
